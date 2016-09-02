8dd5a04a4cec40aea706cfe30eabe030814414eb
Shaun Martin <webmaster@ginblades.com>
2016-09-01 21:03:34 -0500

    Reorganize for my own preference.

---
+++ SimpleBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models {
+    public class Post {
+        [Required]
+        public int PostId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public DateTime Publish { get; set; }
+        [Required]
+        public PostStatus PostStatus { get; set; }
+        [Required]
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public enum PostStatus {
+        Draft, Review, Publish, Archive
+    }
+}
---
+++ SimpleBlog/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models {
+    public class Tag {
+        [Required]
+        public int TagId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
