7e76fc0809e16ac544311cdd89b3a05eb05faf72
Shaun Martin <webmaster@ginblades.com>
2016-09-03 20:03:42 -0500

    Add Post scaffold.

---
+++ SimpleBlog/Controllers/PostsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlog.Models;
+
+namespace SimpleBlog.Controllers {
+
+    [Authorize]
+    public class PostsController : Controller {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Posts
+        public async Task<ActionResult> Index() {
+            return View(await db.Post.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<ActionResult> Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Post.FindAsync(id);
+            if (post == null) {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PostId,Title,Body,Publish,PostStatus,ApplicationUserId")] Post post) {
+            if (ModelState.IsValid) {
+                db.Post.Add(post);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<ActionResult> Edit(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Post.FindAsync(id);
+            if (post == null) {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "PostId,Title,Body,Publish,PostStatus,ApplicationUserId")] Post post) {
+            if (ModelState.IsValid) {
+                db.Entry(post).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<ActionResult> Delete(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = await db.Post.FindAsync(id);
+            if (post == null) {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id) {
+            Post post = await db.Post.FindAsync(id);
+            db.Post.Remove(post);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- SimpleBlog/Models/Post.cs
+++ SimpleBlog/Models/Post.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace SimpleBlog.Models {
     public class Post {
@@ -11,13 +12,13 @@ namespace SimpleBlog.Models {
         public int PostId { get; set; }
         [Required, MaxLength(256)]
         public string Title { get; set; }
-        [Required]
+        [Required, DataType(DataType.MultilineText)]
         public string Body { get; set; }
-        [Required]
+        [Required, DataType(DataType.DateTime)]
         public DateTime Publish { get; set; }
         [Required]
         public PostStatus PostStatus { get; set; }
-        [Required]
+        [Required, HiddenInput(DisplayValue = false)]
         public string ApplicationUserId { get; set; }
         [NotMapped]
         public ApplicationUser ApplicationUser { get; set; }
